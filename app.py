7ad06f8c3c0365eb2f362a8dd73076ade4239264
aoshenye <aoshenye@gmail.com>
2021-05-28 13:04:26 +0000

    modify bar colors

--- app.py
+++ app.py
@@ -9,27 +9,27 @@ app.config['SECRET_KEY'] = 'thismaybeasecret!'
 
 @app.route("/")
 def template_test():
-    return render_template('template.html')
+    return render_template('layout.html', title='Home')
 
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('login.html', title='Login')
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("register.html", title='Register')
 
 
 @app.route("/profile")
 def profile():
-    return render_template("profile.html")
+    return render_template("profile.html", title='Profile')
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title='About')
 
 
 if __name__ == '__main__':
