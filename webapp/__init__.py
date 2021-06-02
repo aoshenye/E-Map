0d372ee3ebaf1f84eef870e36512b851ecc2e91e
aoshenye <aoshenye@gmail.com>
2021-06-02 23:37:39 +0000

    fixed errors need to add old html back in

---
+++ webapp/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '643e449d5787be04d70700f8e3625070'
+
+    from.views import views
+    from.auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
