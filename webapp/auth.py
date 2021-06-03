6bfa8c62a56293de461fdf1ca985399313a26d05
aoshenye <aoshenye@gmail.com>
2021-06-03 12:39:27 +0000

    finish handlign post requests

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", text="Testing", boolean=True)
+    return render_template("login.html", boolean=True)
 
 
 @auth.route('/logout')
@@ -13,6 +13,23 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            pass
+        elif len(firstName) < 2:
+            pass
+        elif password1 != password2:
+            pass
+        elif len(password1) < 7:
+            pass
+        else:
+
     return render_template("sign_up.html")
+
