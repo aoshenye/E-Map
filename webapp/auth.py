bbd90dafac465822c14ba1696cc4dbcb64d7d6fd
aoshenye <aoshenye@gmail.com>
2021-06-03 12:20:43 +0000

    creat forms

--- webapp/auth.py
+++ webapp/auth.py
@@ -5,7 +5,7 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template("login.html", text="Testing", boolean=True)
 
 
 @auth.route('/logout')
