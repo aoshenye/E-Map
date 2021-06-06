575bf6e92d7d118e42af0bdf3c0b62203ccc553a
aoshenye <aoshenye@gmail.com>
2021-06-06 22:59:08 +0000

    create database and models

--- webapp/auth.py
+++ webapp/auth.py
@@ -17,18 +17,19 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstname')
+        first_name = request.form.get('first_name')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('First name must be greater than 1 char.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
+            flash('Account created!', category='success')
 
     return render_template("sign_up.html")
