090972835d9f3b6407d49140d56dab0cfc18453f
aoshenye <aoshenye@gmail.com>
2021-06-10 20:23:42 +0000

    clean up sign up error- add about us page and view

--- webapp/views.py
+++ webapp/views.py
@@ -8,3 +8,8 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+
+@views.route('/aboutus')
+def aboutus():
+    return render_template("aboutus.html", user=current_user)
