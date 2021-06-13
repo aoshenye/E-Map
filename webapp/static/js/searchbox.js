4916f86a32035cbf70585f95c119996cb16e55d8
aoshenye <aoshenye@gmail.com>
2021-06-13 23:08:35 +0000

    add map

--- webapp/static/js/searchbox.js
+++ webapp/static/js/searchbox.js
@@ -10,35 +10,55 @@ var infowindowContent;
 var mapPlaceMarker;
 var service;
 
+let map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-        center: initialLocation,
-        zoom: 13,
+        zoom: 2,
+        center: new google.maps.LatLng(2.8, -187.3),
+        mapTypeId: "terrain",
     });
+    // Create a <script> tag and set the USGS URL as the source.
+    const script = document.createElement("script");
+    // This example uses a local copy of the GeoJSON stored at
+    // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
+    script.src =
+        "https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
 
-    const input = document.getElementById("pac-input");
-    autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo("bounds", map);
+// Loop through the results array and place a marker for each
+// set of coordinates.
+const eqfeed_callback = function(results) {
+    for (let i = 0; i < results.features.length; i++) {
+        const coords = results.features[i].geometry.coordinates;
+        const latLng = new google.maps.LatLng(coords[1], coords[0]);
+        new google.maps.Marker({
+            position: latLng,
+            map: map,
+        });
+    }
+};
 
-    // Specify just the place data fields that you need.
-    autocomplete.setFields(["place_id", "geometry", "name", "formatted_address"]);
+// Specify just the place data fields that you need.
+autocomplete.setFields(["place_id", "geometry", "name", "formatted_address"]);
 
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    infowindow = new google.maps.InfoWindow();
-    infowindowContent = document.getElementById("infowindow-content");
-    infowindow.setContent(infowindowContent);
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+infowindow = new google.maps.InfoWindow();
+infowindowContent = document.getElementById("infowindow-content");
+infowindow.setContent(infowindowContent);
 
-    mapPlaceMarker = new google.maps.Marker({
-        map: map
-    });
-    mapPlaceMarker.addListener("click", () => {
-        infowindow.open(map, marker);
-    });
+mapPlaceMarker = new google.maps.Marker({
+    map: map
+});
+mapPlaceMarker.addListener("click", () => {
+    infowindow.open(map, marker);
+});
 
-    // Places API service
-    service = new google.maps.places.PlacesService(map);
+// Places API service
+service = new google.maps.places.PlacesService(map);
 
-    autocomplete.addListener("place_changed", autocompleteCallback);
+autocomplete.addListener("place_changed", autocompleteCallback);
 }
 
 /* 
@@ -78,81 +98,80 @@ function createMarkersAndUpdateHTML(places, map) {
             title: place.name,
             position: place.geometry.location,
         });
-        
+
         // Add callback to the marker here
 
-function initMap() {
+        function initMap() {
 
-    var latlng = new google.maps.LatLng(28.535516,77.391026);
+            var latlng = new google.maps.LatLng(28.535516, 77.391026);
 
-    if (navigator.geolocation) {
-      try {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          var myLocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-          setPos(myLocation);
-        });
-      } catch (err) {
-        var myLocation = {
-          lat: 25.2048,
-          lng: 55.2708
-        };
-        setPos(myLocation);
-      }
-    } else {
-      var myLocation = {
-        lat: 25.2048,
-        lng: 55.2708
-      };
-      setPos(myLocation);
-    }
-  }
+            if (navigator.geolocation) {
+                try {
+                    navigator.geolocation.getCurrentPosition(function(position) {
+                        var myLocation = {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude
+                        };
+                        setPos(myLocation);
+                    });
+                } catch (err) {
+                    var myLocation = {
+                        lat: 25.2048,
+                        lng: 55.2708
+                    };
+                    setPos(myLocation);
+                }
+            } else {
+                var myLocation = {
+                    lat: 25.2048,
+                    lng: 55.2708
+                };
+                setPos(myLocation);
+            }
+        }
 
-  function setPos(myLocation) {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: myLocation,
-      zoom: 10
-    });
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: myLocation,
-      radius: 5000,
-      types: ['night_club']
-    }, processResults);
-  }
+        function setPos(myLocation) {
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: myLocation,
+                zoom: 10
+            });
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: myLocation,
+                radius: 5000,
+                types: ['night_club']
+            }, processResults);
+        }
 
-  function processResults(results, status, pagination) {
-    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-      return;
-    } else {
-      createMarkers(results);
-    }
-  }
+        function processResults(results, status, pagination) {
+            if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                return;
+            } else {
+                createMarkers(results);
+            }
+        }
 
-  function createMarkers(places) {
-    var bounds = new google.maps.LatLngBounds();
-    var placesList = document.getElementById('places');
-    for (var i = 0, place; place = places[i]; i++) 
-    {
-     
-      var marker = new google.maps.Marker({
-        map: map,
-        icon: image,
-        title: place.name,
-        animation: google.maps.Animation.DROP,
-        position: place.geometry.location
-      });
+        function createMarkers(places) {
+            var bounds = new google.maps.LatLngBounds();
+            var placesList = document.getElementById('places');
+            for (var i = 0, place; place = places[i]; i++) {
 
-      bounds.extend(place.geometry.location);
-    }
-    map.fitBounds(bounds);
-  }
+                var marker = new google.maps.Marker({
+                    map: map,
+                    icon: image,
+                    title: place.name,
+                    animation: google.maps.Animation.DROP,
+                    position: place.geometry.location
+                });
 
-// To add the marker to the map, call setMap();
-marker.setMap(map);
-        
+                bounds.extend(place.geometry.location);
+            }
+            map.fitBounds(bounds);
+        }
+
+        // To add the marker to the map, call setMap();
+        marker.setMap(map);
+
         bounds.extend(place.geometry.location);
     }
     map.fitBounds(bounds);
@@ -210,69 +229,66 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Adding and removing "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    acc[i].addEventListener("click", function() {
+        /* Adding and removing "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-    /* Hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
+        /* Hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
 
 /* Initializing promise of response */
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Hilma").html("<h1>Salsa for Beginners 1800 - 1930</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#SpyBar").html("<h1>Dj Ivan classics 21-3</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Sodra").html("<h1>Hip Hop & R&B with dj Messi from 2200 - 500</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Slak").html("<h1>Jazz night with special guest perfomances 1900 - 2300</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Fash").html("<h1>Flash mob part with Kaz & Jeff 2100 - 0000</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Trad").html("<h1>Field Play events 1700 - 2100</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Hobo").html("<h1> Hotel Selections presents... 2100 - 300</h1>");
     });
 });
 
-$(document).ready(function(){
-    $("button").click(function(){
+$(document).ready(function() {
+    $("button").click(function() {
         $("#Himlen").html("<h1>After Work 1800 - 0000</h1>");
     });
 });
-
-
- 
